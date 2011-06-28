d03041a660c8f61d5ac0040ce56fd3bae8b5fa1c
Catalin David <catalindavid2003@gmail.com>
2011-06-28 17:33:20 +0200

    further updates to local comments

--- sites/all/modules/local_comments/js/test.js
+++ sites/all/modules/local_comments/js/test.js
@@ -1,31 +1,32 @@
 (function($) {
-	$("body").ready(function() {
-		var link = '<a href="#discuss" class="use-ajax ajax-processed"> New Discussion </a>'
+    $("body").ready(function() {
+	    var link = '<a href="#discuss" class="use-ajax ajax-processed"> New Discussion </a>'
 		var discuss = $("<li>").append(link);
-		var contextMenu = $("<ul>").attr("id",
-				"localDiscussContextMenu").attr("class", "contextMenu").append(
-				discuss);
-		var contextMenuWrapper = $("<div>").append(contextMenu);
-		console.log(Drupal.ajax);
-		$("body").append(contextMenuWrapper);
-		
-		 $(".commentable").contextMenu({
-		        menu: "localDiscussContextMenu"
-		    },
-		        function(action, el, pos) {
-		    	$.ajax({
-		    			url: "/testmod/view/4", 
-		    			success: function(data) {
-		    				console.log(data);
-		    			}
+	    var contextMenu = $("<ul>").attr("id",
+					     "localDiscussContextMenu").attr("class", "contextMenu").append(
+													    discuss);
+	    var contextMenuWrapper = $("<div>").append(contextMenu);
+	    console.log(Drupal.ajax);
+	    $("body").append(contextMenuWrapper);
+
+	    $(".commentable").contextMenu({
+		    menu: "localDiscussContextMenu"
+			},
+		function(action, el, pos) {
+		    $.ajax({
+			    url: "/testmod/view/4",
+				success: function(data) {
+				console.log(data);
+			    }
 		    	});
-		    	alert(
-		            "Action: " + action + "\n\n" +
-		            "Element ID: " + $(el).attr("id") + "\n\n" +
-		            "X: " + pos.x + "  Y: " + pos.y + " (relative to element)\n\n" +
-		            "X: " + pos.docX + "  Y: " + pos.docY+ " (relative to document)"
-		            );
-		    });
+		    $('#edit-xml-id').val($(el).attr('id'));
+		    alert(
+			  "Action: " + action + "\n\n" +
+			  "Element ID: " + $(el).attr("id") + "\n\n" +
+			  "X: " + pos.x + "  Y: " + pos.y + " (relative to element)\n\n" +
+			  "X: " + pos.docX + "  Y: " + pos.docY+ " (relative to document)"
+			  );
+		});
 
 	});
 
