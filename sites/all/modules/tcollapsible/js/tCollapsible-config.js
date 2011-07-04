07c6ec389d2ffe7390479ee84870a1f4cd2d075a
Constantin Jucovschi <jucovschi@gmail.com>
2011-07-04 13:02:50 +0200

    making only the write-up collapsible

--- sites/all/modules/tcollapsible/js/tCollapsible-config.js
+++ sites/all/modules/tcollapsible/js/tCollapsible-config.js
@@ -1,6 +1,6 @@
 jQuery(function(){
 
    var $ = jQuery;
-   $('#block-system-main').find('div.field-items, div.para, table.equation').tCollapsible();
+   $('#block-system-main').find('.field-name-field-writeup div.field-items, .field-name-field-writeup div.para, .field-name-field-writeup table.equation').tCollapsible();
    
 });
