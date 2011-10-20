aa8347dbf6229b186306bac5dd9879b8aab658a1
Constantin Jucovschi <jucovschi@gmail.com>
2011-10-20 09:49:23 +0100

    integrated etherpad to drupal

---
+++ sites/all/modules/etherpad_lite/etherpad_lite_pad.tpl.php
@@ -0,0 +1,6 @@
+<div id="<?php echo $padname; ?>" class="pad" style="width: 700px; height: 500px; ">
+</div>
+
+<script lang="javascript">
+$('#<?php echo $padname;?>').pad({'padId':'<?php echo $padid; ?>','host':'<?php echo $base;?>', 'showChat':'true'});
+</script>
