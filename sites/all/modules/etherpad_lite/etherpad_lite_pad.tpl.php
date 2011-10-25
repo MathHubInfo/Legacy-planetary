0fae5bfac122155f4969e4fca2f3194236e43db6
Constantin Jucovschi <jucovschi@gmail.com>
2011-10-25 18:01:10 +0200

    made etherlite into a wysiwyg module

--- sites/all/modules/etherpad_lite/etherpad_lite_pad.tpl.php
+++
@@ -1,6 +0,0 @@
-<div id="<?php echo $padname; ?>" class="pad" style="width: 700px; height: 500px; ">
-</div>
-
-<script lang="javascript">
-$('#<?php echo $padname;?>').pad({'padId':'<?php echo $padid; ?>','host':'<?php echo $base;?>', 'showChat':'true'});
-</script>
