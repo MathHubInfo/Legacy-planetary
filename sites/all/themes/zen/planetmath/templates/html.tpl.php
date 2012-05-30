dd91442ae818c2b4bab658b2a00dcd0a577f20ad
Joe Corneli <holtzermann17@gmail.com>
2012-05-30 16:34:37 -0400

    Make html.tpl.php RDFa 1.1 compliant

--- sites/all/themes/zen/planetmath/templates/html.tpl.php
+++ sites/all/themes/zen/planetmath/templates/html.tpl.php
@@ -59,9 +59,11 @@
  * @see zen_preprocess_html()
  * @see template_process()
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+?>
+<?php print $doctype; ?>
+
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"
+<?php print $rdf->version . $rdf->namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
   <?php print $head; ?>
