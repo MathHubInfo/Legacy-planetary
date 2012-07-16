f0a3db77485a0d2e6a668934b4f9a981139df6b7
root <root@costea-laptop.(none)>
2012-07-16 10:42:01 +0200

    update codemirror to last version and changes some colors

--- sites/all/libraries/codemirror2/test/test.js
+++ sites/all/libraries/codemirror2/test/test.js
@@ -78,7 +78,7 @@ testCM("indent", function(cm) {
   cm.setOption("indentUnit", 8);
   cm.indentLine(1);
   eq(cm.getLine(1), "\tblah();");
-}, {value: "if (x) {\nblah();\n}", indentUnit: 3, indentWithTabs: true});
+}, {value: "if (x) {\nblah();\n}", indentUnit: 3, indentWithTabs: true, tabSize: 8});
 
 test("defaults", function() {
   var olddefaults = CodeMirror.defaults, defs = CodeMirror.defaults = {};
@@ -94,7 +94,7 @@ test("defaults", function() {
     eq(defs.indentUnit, 5);
     eq(cm.getValue(), "uu");
     eq(cm.getOption("enterMode"), "keep");
-    eq(cm.getInputField().tabindex, 55);
+    eq(cm.getInputField().tabIndex, 55);
   }
   finally {
     CodeMirror.defaults = olddefaults;
@@ -116,7 +116,7 @@ testCM("lineInfo", function(cm) {
 }, {value: "111111\n222222\n333333"});
 
 testCM("coords", function(cm) {
-  var scroller = cm.getWrapperElement().getElementsByClassName("CodeMirror-scroll")[0];
+  var scroller = cm.getScrollerElement();
   scroller.style.height = "100px";
   var content = [];
   for (var i = 0; i < 200; ++i) content.push("------------------------------" + i);
@@ -126,8 +126,7 @@ testCM("coords", function(cm) {
   is(top.x < bot.x);
   is(top.y < bot.y);
   is(top.y < top.yBot);
-  scroller.scrollTop = 100;
-  cm.refresh();
+  cm.scrollTo(null, 100);
   var top2 = cm.charCoords({line: 0, ch: 0});
   is(top.y > top2.y);
   eq(top.x, top2.x);
@@ -137,14 +136,42 @@ testCM("coordsChar", function(cm) {
   var content = [];
   for (var i = 0; i < 70; ++i) content.push("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
   cm.setValue(content.join("\n"));
-  for (var x = 0; x < 35; x += 2) {
-    for (var y = 0; y < 70; y += 5) {
-      cm.setCursor(y, x);
-      var pos = cm.coordsChar(cm.charCoords({line: y, ch: x}));
-      eq(pos.line, y);
-      eq(pos.ch, x);
+  for (var ch = 0; ch < 35; ch += 2) {
+    for (var line = 0; line < 70; line += 5) {
+      cm.setCursor(line, ch);
+      var coords = cm.charCoords({line: line, ch: ch});
+      var pos = cm.coordsChar({x: coords.x, y: coords.y + 1});
+      eq(pos.line, line);
+      eq(pos.ch, ch);
     }
   }
+});
+
+testCM("posFromIndex", function(cm) {
+  cm.setValue(
+    "This function should\n" +
+    "convert a zero based index\n" +
+    "to line and ch."
+  );
+
+  var examples = [
+    { index: -1, line: 0, ch: 0  }, // <- Tests clipping
+    { index: 0,  line: 0, ch: 0  },
+    { index: 10, line: 0, ch: 10 },
+    { index: 39, line: 1, ch: 18 },
+    { index: 55, line: 2, ch: 7  },
+    { index: 63, line: 2, ch: 15 },
+    { index: 64, line: 2, ch: 15 }  // <- Tests clipping
+  ];
+
+  for (var i = 0; i < examples.length; i++) {
+    var example = examples[i];
+    var pos = cm.posFromIndex(example.index);
+    eq(pos.line, example.line);
+    eq(pos.ch, example.ch);
+    if (example.index >= 0 && example.index < 64)
+      eq(cm.indexFromPos(pos), example.index);
+  }  
 });
 
 testCM("undo", function(cm) {
@@ -159,22 +186,17 @@ testCM("undo", function(cm) {
   eq(cm.historySize().undo, 1);
   eq(cm.historySize().redo, 0);
   cm.setValue("1\n\n\n2");
+  cm.clearHistory();
   eq(cm.historySize().undo, 0);
   for (var i = 0; i < 20; ++i) {
     cm.replaceRange("a", {line: 0, ch: 0});
     cm.replaceRange("b", {line: 3, ch: 0});
   }
   eq(cm.historySize().undo, 40);
-  for (var i = 0; i < 38; ++i) cm.undo();
-  eq(cm.historySize().undo, 2);
-  eq(cm.historySize().redo, 38);
-  eq(cm.getValue(), "a1\n\n\nb2");
-  cm.setOption("undoDepth", 10);
-  for (var i = 0; i < 20; ++i) {
-    cm.replaceRange("a", {line: 0, ch: 0});
-    cm.replaceRange("b", {line: 3, ch: 0});
-  }
-  eq(cm.historySize().undo, 10);
+  for (var i = 0; i < 40; ++i)
+    cm.undo();
+  eq(cm.historySize().redo, 40);
+  eq(cm.getValue(), "1\n\n\n2");
 }, {value: "abc"});
 
 testCM("undoMultiLine", function(cm) {
@@ -192,6 +214,77 @@ testCM("undoMultiLine", function(cm) {
   cm.undo();
   eq(cm.getValue(), "abc\ndef\nghi");
 }, {value: "abc\ndef\nghi"});
+
+testCM("markTextSingleLine", function(cm) {
+  forEach([{a: 0, b: 1, c: "", f: 2, t: 5},
+           {a: 0, b: 4, c: "", f: 0, t: 2},
+           {a: 1, b: 2, c: "x", f: 3, t: 6},
+           {a: 4, b: 5, c: "", f: 3, t: 5},
+           {a: 4, b: 5, c: "xx", f: 3, t: 7},
+           {a: 2, b: 5, c: "", f: 2, t: 3},
+           {a: 2, b: 5, c: "abcd", f: 6, t: 7},
+           {a: 2, b: 6, c: "x", f: null, t: null},
+           {a: 3, b: 6, c: "", f: null, t: null},
+           {a: 0, b: 9, c: "hallo", f: null, t: null},
+           {a: 4, b: 6, c: "x", f: 3, t: 4},
+           {a: 4, b: 8, c: "", f: 3, t: 4},
+           {a: 6, b: 6, c: "a", f: 3, t: 6},
+           {a: 8, b: 9, c: "", f: 3, t: 6}], function(test) {
+    cm.setValue("1234567890");
+    var r = cm.markText({line: 0, ch: 3}, {line: 0, ch: 6}, "foo");
+    cm.replaceRange(test.c, {line: 0, ch: test.a}, {line: 0, ch: test.b});
+    var f = r.find();
+    eq(f.from && f.from.ch, test.f); eq(f.to && f.to.ch, test.t);
+  });
+});
+
+testCM("markTextMultiLine", function(cm) {
+  function p(v) { return v && {line: v[0], ch: v[1]}; }
+  forEach([{a: [0, 0], b: [0, 5], c: "", f: [0, 0], t: [2, 5]},
+           {a: [0, 1], b: [0, 10], c: "", f: [0, 1], t: [2, 5]},
+           {a: [0, 5], b: [0, 6], c: "x", f: [0, 6], t: [2, 5]},
+           {a: [0, 0], b: [1, 0], c: "", f: [0, 0], t: [1, 5]},
+           {a: [0, 6], b: [2, 4], c: "", f: [0, 5], t: [0, 7]},
+           {a: [0, 6], b: [2, 4], c: "aa", f: [0, 5], t: [0, 9]},
+           {a: [1, 2], b: [1, 8], c: "", f: [0, 5], t: [2, 5]},
+           {a: [0, 5], b: [2, 5], c: "xx", f: null, t: null},
+           {a: [0, 0], b: [2, 10], c: "x", f: null, t: null},
+           {a: [1, 5], b: [2, 5], c: "", f: [0, 5], t: [1, 5]},
+           {a: [2, 0], b: [2, 3], c: "", f: [0, 5], t: [2, 2]},
+           {a: [2, 5], b: [3, 0], c: "a\nb", f: [0, 5], t: [2, 5]},
+           {a: [2, 3], b: [3, 0], c: "x", f: [0, 5], t: [2, 4]},
+           {a: [1, 1], b: [1, 9], c: "1\n2\n3", f: [0, 5], t: [4, 5]}], function(test) {
+    cm.setValue("aaaaaaaaaa\nbbbbbbbbbb\ncccccccccc\ndddddddd\n");
+    var r = cm.markText({line: 0, ch: 5}, {line: 2, ch: 5}, "foo");
+    cm.replaceRange(test.c, p(test.a), p(test.b));
+    var f = r.find();
+    eqPos(f.from, p(test.f)); eqPos(f.to, p(test.t));
+  });
+});
+
+testCM("bookmark", function(cm) {
+  function p(v) { return v && {line: v[0], ch: v[1]}; }
+  forEach([{a: [1, 0], b: [1, 1], c: "", d: [1, 4]},
+           {a: [1, 1], b: [1, 1], c: "xx", d: [1, 7]},
+           {a: [1, 4], b: [1, 5], c: "ab", d: [1, 6]},
+           {a: [1, 4], b: [1, 6], c: "", d: null},
+           {a: [1, 5], b: [1, 6], c: "abc", d: [1, 5]},
+           {a: [1, 6], b: [1, 8], c: "", d: [1, 5]},
+           {a: [1, 4], b: [1, 4], c: "\n\n", d: [3, 1]},
+           {bm: [1, 9], a: [1, 1], b: [1, 1], c: "\n", d: [2, 8]}], function(test) {
+    cm.setValue("1234567890\n1234567890\n1234567890");
+    var b = cm.setBookmark(p(test.bm) || {line: 1, ch: 5});
+    cm.replaceRange(test.c, p(test.a), p(test.b));
+    eqPos(b.find(), p(test.d));
+  });
+});
+
+testCM("bug577", function(cm) {
+  cm.setValue("a\nb");
+  cm.clearHistory();
+  cm.setValue("fooooo");
+  cm.undo();
+});
 
 // Scaffolding
 
@@ -239,6 +332,8 @@ function eq(a, b, msg) {
   if (a != b) throw new Failure(a + " != " + b + (msg ? " (" + msg + ")" : ""));
 }
 function eqPos(a, b, msg) {
+  if (a == b) return;
+  if (a == null || b == null) throw new Failure("comparing point to null");
   eq(a.line, b.line, msg);
   eq(a.ch, b.ch, msg);
 }
