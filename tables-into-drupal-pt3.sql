8ea1399924d7f10a30feb4811a81946cbe440aba
Joe Corneli <holtzermann17@gmail.com>
2012-08-13 11:48:43 +0100

    Modify the treatment and post-processing of latex fields

--- tables-into-drupal-pt3.sql
+++ tables-into-drupal-pt3.sql
@@ -1,5 +1,6 @@
--- Some revisions are currently not handled by migrate --
---  tables-into-drupal-pt2.sq
+-- Some stuff that is not currently handled by the standard migrate module --
+
+-- Note: remember to clear the cache (drush cc all) when this is finished
 
 -- This updates the total without recording a specific transaction bringing
 -- things up to date.  But hey, at least the totals are right.
@@ -7,17 +8,25 @@ UPDATE userpoints SET points = (SELECT score FROM planetmath_users WHERE planetm
 
 UPDATE userpoints_total SET points = (SELECT score FROM planetmath_users WHERE planetmath_users.uid = userpoints_total.uid), max_points=points;
 
+-- Copy the article latex over
+
 INSERT INTO field_data_field_latex
  (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_latex_preamble, field_latex_document,field_latex_format)
  SELECT 'node','article',0, node.nid, node.vid,'und',0, planetmath_objects.preamble, planetmath_objects.data, 'tex_editor'
  FROM planetmath_objects,node where node.nid = planetmath_objects.uid;
 
-INSERT INTO field_data_field_question_latex
- (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_question_latex_document,field_question_latex_preamble,field_question_latex_format)
- SELECT 'node','question',0, node.nid, node.vid,'und',0,data,'','tex_editor'
- FROM planetmath_requests,node where node.nid = planetmath_requests.uid;
+-- Copy the collab latex over
 
 INSERT INTO field_data_field_latex
  (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_latex_preamble, field_latex_document,field_latex_format)
- SELECT 'node','article',0, node.nid, node.vid,'und',0, '', planetmath_collab.data, 'tex_editor'
+ SELECT 'node','article',0, node.nid, node.vid,'und',0, '%none for now', planetmath_collab.data, 'tex_editor'
  FROM planetmath_collab,node where node.nid = planetmath_collab.uid;
+
+
+-- Copy the data for questions over
+
+INSERT INTO field_data_field_question_latex
+ (entity_type, bundle, deleted, entity_id, revision_id, language, delta, field_question_latex_document,field_question_latex_preamble,field_question_latex_metadata,field_question_latex_format)
+ SELECT 'node','question',0, node.nid, node.vid,'und',0,planetmath_requests.data,'%none for now','','tex_editor'
+ FROM planetmath_requests,node where node.nid = planetmath_requests.uid;
+
