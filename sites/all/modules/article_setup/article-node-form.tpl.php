564eef3bfd9667c789be49e4fbcf11d7cca95032
Joe Corneli <holtzermann17@gmail.com>
2012-09-22 10:22:20 -0400

    Solve most of #204

--- sites/all/modules/article_setup/article-node-form.tpl.php
+++ sites/all/modules/article_setup/article-node-form.tpl.php
@@ -2,7 +2,6 @@
     <div class="node-column-main">
         <?php
         global $user;
-        //dd($user);
         ?>
         <?php if ($form): ?>
            <?php if (0!=strcmp("CanonicalName",$form['field_canonicalname']['und'][0]['value']['#default_value'] ) ) :  ?>
@@ -13,6 +12,36 @@
              </h3>
             <?php endif; ?>
             <?php hide($form['field_canonicalname']); ?>
+            <?php print render($form['title']);
+                  print render($form['field_latex']);
+                  // Only display the revision comment if the article has been
+                  // "changed" (i.e. saved).
+                  if($form['changed']['#value']!='') {
+                    print render($form['field_revisioncomment']);
+                  }
+                  print render($form['field_mathtype']);
+                  print render($form['field_msc']);
+                  print render($form['field_parent']);
+            // I'm putting the next bit into a div, so that we can later hide/show it
+            // like we did with the preamble, to make things simpler for users
+           ?>
+            <div id="thesaurus">
+		  <i>Defines</i>, <i>Keywords</i>, <i>Related</i>, and <i>Synonym</i> metadata can help people find this article, but are not required.  See the FAQ for more details.
+            <?php print render($form['field_defines']);
+                  print render($form['field_keywords']);
+                  print render($form['field_related']);
+                  print render($form['field_synonym']);
+            ?>
+            </div>
+            <?php print render($form['field_section']); 
+	      // only show the "site doc" field to people with the "admin role"
+                  if ((is_array($user->roles) 
+		       && in_array('administrator', $user->roles))) {
+                  print render($form['field_sitedoc']);
+		  } else {
+		    hide($form['field_sitedoc']);
+		  }
+                  print render($form['field_published']); ?>
             <?php print drupal_render_children($form); ?>
         <?php endif; ?>
     </div>
