cc08b1a2340815806ef44fd72f2f8817b6c0ef05
Joe Corneli <holtzermann17@gmail.com>
2012-10-23 13:52:28 -0400

    Move javascript for hiding/showing the thesaurus metadata, to get rid of the intial 'bounce' effect

--- sites/all/modules/article_setup/article-node-form.tpl.php
+++ sites/all/modules/article_setup/article-node-form.tpl.php
@@ -40,7 +40,7 @@
     ?>
             <div class="thesaurus">
             <label href="#" class="show_hide">Show metadata »</label>
-            <div class="thesaurus_content">
+            <div class="thesaurus_content" style="display:none;">
    	       <i>Defines</i>, <i>Keywords</i>, <i>Related</i>, and <i>Synonym</i> metadata can help people find this article, but are not required.  See the FAQ for more details.
             <?php print render($form['field_defines']);
                   print render($form['field_keywords']);
@@ -55,32 +55,3 @@
     <div class="clear"></div>
 </div>
 
-<script type="text/javascript">
-  (function($){
-	   $(document).ready(function(){
-	       $('.thesaurus').each(function(){
-		   var dataShown =0;
-		   var data = $(this);
-		   console.log(data);
-		   data.find('label.show_hide')
-		       .text('Show thesaurus metadata »')
-		       .css('color','#06799F')
-        	       .css('cursor','pointer');
-		   data.find('div').hide;
-		   data.find('label.show_hide').click(function(){
-		       data.find('div').eq(0).toggle(500,function(){
-			   console.log(dataShown);
-			   if (dataShown==0){
-			       data.find('label.show_hide').text('Show thesaurus metadata »');
-			       dataShown=1;
-			   } else {
-			       data.find('label.show_hide').text('« Hide thesaurus metadata');
-			       dataShown=0;
-			   }
-		       }); 
-		   });
-});
-$('label.show_hide').click();
-});
-})(jQuery);
-</script>
