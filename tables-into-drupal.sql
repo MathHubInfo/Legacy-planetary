c8629ccfa62fe816e45972b0e5024c82a3998bdd
Joe Corneli <holtzermann17@gmail.com>
2012-11-01 07:26:38 -0400

    revert tables-into-drupal.sql to template

--- tables-into-drupal.sql
+++ tables-into-drupal.sql
@@ -1,5 +1,5 @@
--- Note: replace "planetaryRC1" in this file with the name of your actual drupal database!
--- sed -i 's/planetaryRC1/real-database-name/g' tables-into-drupal.sql
+-- Note: replace "drupaldb" in this file with the name of your actual drupal database!
+-- sed -i 's/drupaldb/real-database-name/g' tables-into-drupal.sql
 
 -- Assuming you've got a Noosphere dump, first run something like this to import it:
 
@@ -9,58 +9,58 @@
 
 -- Then moving content by loading this file.
 -- The simplest way to do this is:
--- $ mysql -u root --password=PASSWORD planetaryRC1 < tables-into-drupal.sql
+-- $ mysql -u root --password=PASSWORD drupaldb < tables-into-drupal.sql
 
--- (Note that you *could* give access to the planetary database to the planetaryRC1 user,
+-- (Note that you *could* give access to the planetary database to the drupaldb user,
 -- but that's an extra configuration step.)
 
 -- Next: you will have to do further configurations, which are in
 -- tables-into-drupal-pt2.sql and tables-into-drupal-pt3.sql
 
--- Those can be run as the planetaryRC1 user.  Details follow:
+-- Those can be run as the drupaldb user.  Details follow:
 
 -- BEFORE Drupal migrations:
--- $ mysql -u planetaryRC1 --password=PASSWORD planetaryRC1 < tables-into-drupal-pt2.sql
+-- $ mysql -u drupaldb --password=PASSWORD drupaldb < tables-into-drupal-pt2.sql
 
 -- AFTER Drupal migrations:
--- $ mysql -u planetaryRC1 --password=PASSWORD planetaryRC1 < tables-into-drupal-pt3.sql
+-- $ mysql -u drupaldb --password=PASSWORD drupaldb < tables-into-drupal-pt3.sql
 
 -- See those files for specific commands.
 
-CREATE TABLE planetaryRC1.planetmath_users LIKE staging.users;
-INSERT INTO planetaryRC1.planetmath_users SELECT * FROM staging.users;
+CREATE TABLE drupaldb.planetmath_users LIKE planetary.users;
+INSERT INTO drupaldb.planetmath_users SELECT * FROM planetary.users;
 
-CREATE TABLE planetaryRC1.planetmath_objects LIKE staging.objects;
-INSERT INTO planetaryRC1.planetmath_objects SELECT * FROM staging.objects;
+CREATE TABLE drupaldb.planetmath_objects LIKE planetary.objects;
+INSERT INTO drupaldb.planetmath_objects SELECT * FROM planetary.objects;
 
-CREATE TABLE planetaryRC1.planetmath_collab LIKE staging.collab;
-INSERT INTO planetaryRC1.planetmath_collab SELECT * FROM staging.collab;
+CREATE TABLE drupaldb.planetmath_collab LIKE planetary.collab;
+INSERT INTO drupaldb.planetmath_collab SELECT * FROM planetary.collab;
 
-CREATE TABLE planetaryRC1.planetmath_messages LIKE staging.messages;
-INSERT INTO planetaryRC1.planetmath_messages SELECT * FROM staging.messages;
+CREATE TABLE drupaldb.planetmath_messages LIKE planetary.messages;
+INSERT INTO drupaldb.planetmath_messages SELECT * FROM planetary.messages;
 
-CREATE TABLE planetaryRC1.planetmath_acl LIKE staging.acl;
-INSERT INTO planetaryRC1.planetmath_acl SELECT * FROM staging.acl;
+CREATE TABLE drupaldb.planetmath_acl LIKE planetary.acl;
+INSERT INTO drupaldb.planetmath_acl SELECT * FROM planetary.acl;
 
-CREATE TABLE planetaryRC1.planetmath_groups LIKE staging.groups;
-INSERT INTO planetaryRC1.planetmath_groups SELECT * FROM staging.groups;
+CREATE TABLE drupaldb.planetmath_groups LIKE planetary.groups;
+INSERT INTO drupaldb.planetmath_groups SELECT * FROM planetary.groups;
 
-CREATE TABLE planetaryRC1.planetmath_group_members LIKE staging.group_members;
-INSERT INTO planetaryRC1.planetmath_group_members SELECT * FROM staging.group_members;
+CREATE TABLE drupaldb.planetmath_group_members LIKE planetary.group_members;
+INSERT INTO drupaldb.planetmath_group_members SELECT * FROM planetary.group_members;
 
-CREATE TABLE planetaryRC1.planetmath_corrections LIKE staging.corrections;
-INSERT INTO planetaryRC1.planetmath_corrections SELECT * FROM staging.corrections;
+CREATE TABLE drupaldb.planetmath_corrections LIKE planetary.corrections;
+INSERT INTO drupaldb.planetmath_corrections SELECT * FROM planetary.corrections;
 
-CREATE TABLE planetaryRC1.planetmath_requests LIKE staging.requests;
-INSERT INTO planetaryRC1.planetmath_requests SELECT * FROM staging.requests;
+CREATE TABLE drupaldb.planetmath_requests LIKE planetary.requests;
+INSERT INTO drupaldb.planetmath_requests SELECT * FROM planetary.requests;
 
-CREATE TABLE planetaryRC1.planetmath_classification LIKE staging.classification;
-INSERT INTO planetaryRC1.planetmath_classification SELECT * FROM staging.classification;
+CREATE TABLE drupaldb.planetmath_classification LIKE planetary.classification;
+INSERT INTO drupaldb.planetmath_classification SELECT * FROM planetary.classification;
 
-CREATE TABLE planetaryRC1.planetmath_msc LIKE staging.msc;
-INSERT INTO planetaryRC1.planetmath_msc SELECT * FROM staging.msc;
+CREATE TABLE drupaldb.planetmath_msc LIKE planetary.msc;
+INSERT INTO drupaldb.planetmath_msc SELECT * FROM planetary.msc;
 
-CREATE TABLE planetaryRC1.planetmath_authors LIKE staging.authors;
-INSERT INTO planetaryRC1.planetmath_authors SELECT * FROM staging.authors;
+CREATE TABLE drupaldb.planetmath_authors LIKE planetary.authors;
+INSERT INTO drupaldb.planetmath_authors SELECT * FROM planetary.authors;
 
 -- See tables-into-drupal-pt2.sql for the next step
