ef35af25920030cea62fd7ed454893e824fc56f8
Joe Corneli <holtzermann17@gmail.com>
2013-02-12 17:47:17 -0500

    Added a template that accompanies the attachable content module that I had previously forgotten

---
+++ sites/all/themes/zen/planetmath/templates/block--views--acv_attachable_content_block.tpl.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print $content; ?>
+  </div>
+
+</div><!-- /.block -->
