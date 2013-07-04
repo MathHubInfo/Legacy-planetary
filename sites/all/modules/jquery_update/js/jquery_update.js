2b5287b586371341e203f43da8c4f55e9dbf42c0
Constantin Jucovschi <jucovschi@gmail.com>
2013-07-04 15:10:00 +0200

    removing jQuery_update, removing jquery from the library list in planetary libs

--- sites/all/modules/jquery_update/js/jquery_update.js
+++
@@ -1,7 +0,0 @@
-(function (D) {
-  var beforeSerialize = D.ajax.prototype.beforeSerialize;
-  D.ajax.prototype.beforeSerialize = function (element, options) {
-    beforeSerialize.call(this, element, options);
-    options.data['ajax_page_state[jquery_version]'] = D.settings.ajaxPageState.jquery_version;
-  }
-})(Drupal);
