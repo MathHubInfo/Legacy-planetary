4fdd71893b0a7997ffe4281f08e7a729d8014d98
Mihai Fieraru <fierarufmihai@gmail.com>
2013-08-01 19:50:58 +0200

    Project Generation 01.08.2013

---
+++ sites/all/modules/kwarc/project_description.tpl.php
@@ -0,0 +1,35 @@
+
+
+
+<div>
+	<div class="project-desc-img-<?php echo $view_mode?>" >
+	 <?php echo drupal_render($content["field_image"]); ?>
+	</div>
+	
+	<div class="project-desc-desc-<?php echo $view_mode?>" >
+	 <?php echo drupal_render($content["field_project_description"]);?>
+	</div>
+
+
+	<div class="project-desc-small-<?php echo $view_mode?>" >
+	 <?php echo drupal_render($content["field_project_small_description"]);?>
+	</div>
+	
+
+
+	<div class="project-desc-dateandpeople-<?php echo $view_mode?>" >
+
+	<div class="project-desc-people-<?php echo $view_mode?>" >
+	 <?php  echo drupal_render($content["my_additional_field"]); ?>
+	</div>
+
+	<div class="project-desc-date-<?php echo $view_mode?>">
+	 <?php  echo drupal_render($content["field_project_start_date"]); ?>	
+	 <?php  echo drupal_render($content["field_project_end_date"]); ?>
+	</div> 
+
+	</div>
+
+
+
+</div>
